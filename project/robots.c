4c48444b50fafcd92ce90986ee08b538de731757
Giraudux <giraudux@gmail.com>
2015-03-15 19:41:31 +0100

    load matrix and write lp

--- project/robots.c
+++ project/robots.c
@@ -1,4 +1,4 @@
-/* NOM1 Prénom1 - NOM2 Prénom2 */
+/* GIRAUDET Alexis - HALLEREAU François */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,7 +20,7 @@ typedef struct {
 void crono_start()
 {
     struct rusage rusage;
-    
+
     getrusage(RUSAGE_SELF, &rusage);
     start_utime = rusage.ru_utime;
 }
@@ -28,7 +28,7 @@ void crono_start()
 void crono_stop()
 {
     struct rusage rusage;
-    
+
     getrusage(RUSAGE_SELF, &rusage);
     stop_utime = rusage.ru_utime;
 }
@@ -36,7 +36,7 @@ void crono_stop()
 double crono_ms()
 {
     return (stop_utime.tv_sec - start_utime.tv_sec) * 1000 +
-    (stop_utime.tv_usec - start_utime.tv_usec) / 1000 ;
+           (stop_utime.tv_usec - start_utime.tv_usec) / 1000 ;
 }
 
 void lecture_data(char *file, donnees *p)
@@ -74,7 +74,7 @@ int main(int argc, char *argv[])
     donnees p;
     int n, i, j, pos, nbcreux, nbcontr, nbvar;
     double temps, z;
-    int nbsol = 0; /* Compteur du nombre d'appels au solveur GLPK */ 
+    int nbsol = 0; /* Compteur du nombre d'appels au solveur GLPK */
     int * ia;
     int * ja;
     double * ar;
@@ -106,7 +106,7 @@ int main(int argc, char *argv[])
     crono_start(); /* Lancement du compteur (juste après le chargement des données à partir d'un fichier */
 
     glp_prob *prob; /* déclaration du pointeur sur le problème */
-    prob = glp_create_prob(); /* allocation mémoire pour le problème */ 
+    prob = glp_create_prob(); /* allocation mémoire pour le problème */
     glp_set_prob_name(prob, "robots"); /* affectation d'un nom */
     glp_set_obj_dir(prob, GLP_MIN); /* Il s'agit d'un problème de minimisation */
 
@@ -117,11 +117,11 @@ int main(int argc, char *argv[])
     glp_iocp parmip;
     glp_init_iocp(&parmip);
     parmip.msg_lev = GLP_MSG_OFF; /* Paramètre de GLPK dans la résolution d'un PL en variables entières afin de couper les affichages (dans lesquels on se noierait) */
-    
+
     n = p.n;
     nbcontr = 2*n;
     nbvar = n*n;
-    nbcreux = 2*n*(n*n);
+    nbcreux = 2*n*n;
 
     glp_add_rows(prob, nbcontr);
     for(i=1; i<=nbcontr; i++)
@@ -153,12 +153,12 @@ int main(int argc, char *argv[])
 
     pos = 1;
 
-    for(i=0; i<n; i++)
+    for(i=1; i<=n; i++)
     {
         for(j=0; j<n*n; j+=n)
         {
-            ia[pos] = i + 1;
-            ja[pos] = j + i + 1;
+            ia[pos] = i;
+            ja[pos] = j + i;
             ar[pos] = 1.0;
             pos++;
         }
@@ -181,20 +181,26 @@ int main(int argc, char *argv[])
     }
 
     /* Les appels glp_simplex(prob,NULL); et gpl_intopt(prob,NULL); (correspondant aux paramètres par défaut) seront ensuite remplacés par glp_simplex(prob,&parm); et glp_intopt(prob,&parmip); */
-	glp_simplex(prob,NULL);	glp_intopt(prob,NULL); /* Résolution */
-	z = glp_mip_obj_val(prob); /* Récupération de la valeur optimale. Dans le cas d'un problème en variables continues, l'appel est différent : z = glp_get_obj_val(prob);*/
-	x = (double *) malloc (nbvar * sizeof(double));
-	for(i = 0;i < nbvar; i++) x[i] = glp_mip_col_val(prob,i+1); /* Récupération de la valeur des variables, Appel différent dans le cas d'un problème en variables continues : for(i = 0;i < p.nbvar;i++) x[i] = glp_get_col_prim(prob,i+1);	*/
 
-	printf("z = %lf\n",z);
-	for(i=0; i<nbvar; i++) printf("x%d = %d, ",i ,(int)(x[i] + 0.5)); /* un cast est ajouté, x[i] pourrait être égal à 0.99999... */ 
-	puts("");
+    glp_load_matrix(prob, nbcreux, ia, ja, ar);
+
+    glp_write_lp(prob,NULL,"robots.lp");
+
+    glp_simplex(prob,NULL);
+    glp_intopt(prob,NULL); /* Résolution */
+    z = glp_mip_obj_val(prob); /* Récupération de la valeur optimale. Dans le cas d'un problème en variables continues, l'appel est différent : z = glp_get_obj_val(prob);*/
+    x = (double *) malloc (nbvar * sizeof(double));
+    for(i = 0; i < nbvar; i++) x[i] = glp_mip_col_val(prob,i+1); /* Récupération de la valeur des variables, Appel différent dans le cas d'un problème en variables continues : for(i = 0;i < p.nbvar;i++) x[i] = glp_get_col_prim(prob,i+1);	*/
+
+    printf("z = %lf\n",z);
+    for(i=0; i<nbvar; i++) printf("x%d = %d, ",i ,(int)(x[i] + 0.5)); /* un cast est ajouté, x[i] pourrait être égal à 0.99999... */
+    puts("");
     /* A compléter ...
             .
             .
             .
     */
-    
+
     /* Résolution achevée, arrêt du compteur de temps et affichage des résultats */
     crono_stop();
     temps = crono_ms()/1000,0;
@@ -203,7 +209,7 @@ int main(int argc, char *argv[])
     puts("Résultat :");
     puts("-----------");
     /* Affichage de la solution sous la forme d'un cycle avec sa longueur à ajouter */
-    printf("Temps : %f\n", temps);    
+    printf("Temps : %f\n", temps);
     printf("Nombre d'appels à GPLK : %d\n", nbsol);
     printf("Nombre de contraintes ajoutées : %d\n", nbcontr);
 
